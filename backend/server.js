bc2ef547fce464c52cdf67e963439125f5fd4e55
asrithaa06 <thimmampalliasritha@gmail.com>
2026-04-15 22:26:44 +0530

    fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
